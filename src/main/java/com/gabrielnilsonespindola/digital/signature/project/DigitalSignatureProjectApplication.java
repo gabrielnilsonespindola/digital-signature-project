ce33f073246c04245303fc37888750db02cc8352
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-07-10 15:27:04 -0300

    primeiro commit

---
+++ src/main/java/com/gabrielnilsonespindola/digital/signature/project/DigitalSignatureProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielnilsonespindola.digital.signature.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignatureProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignatureProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielnilsonespindola/digital/signature/project/DigitalSignatureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielnilsonespindola.digital.signature.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalSignatureProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
