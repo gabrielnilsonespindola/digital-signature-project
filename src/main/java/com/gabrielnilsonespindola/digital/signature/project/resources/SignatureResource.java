511bf1ae11fab69f0eb602a2fc51b36b64459eda
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-07-10 17:32:29 -0300

    HashService, logica e implementacao SHA 512 aplicada, arquivos da pasta arquivos criados, lombock, logs em console e logback para arquivo criado, bd h2 , atualizacoes maven utf 8 em pom xml

---
+++ src/main/java/com/gabrielnilsonespindola/digital/signature/project/resources/SignatureResource.java
@@ -0,0 +1,8 @@
+package com.gabrielnilsonespindola.digital.signature.project.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SignatureResource {
+
+}
---
+++ src/main/java/com/gabrielnilsonespindola/digital/signature/project/services/HashService.java
@@ -0,0 +1,56 @@
+package com.gabrielnilsonespindola.digital.signature.project.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.stereotype.Service;
+import java.io.File;
+import java.security.MessageDigest;
+import java.security.Security;
+import java.util.Formatter;
+
+@Slf4j
+@Service
+public class HashService {
+
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
+
+	public String calcularHashSHA512(File arquivo) {
+		try {
+			byte[] conteudo = FileUtils.readFileToByteArray(arquivo);
+
+			MessageDigest md = MessageDigest.getInstance("SHA-512", "BC");
+			byte[] hashBytes = md.digest(conteudo);
+
+			String hashHex = bytesParaHex(hashBytes);
+			log.info("Hash SHA-512 calculado com sucesso.");
+			return hashHex;
+
+		} catch (Exception e) {
+			log.error("Erro ao calcular o hash SHA-512 do arquivo", e);
+			throw new RuntimeException("Erro ao calcular hash", e);
+		}
+	}
+
+	private String bytesParaHex(byte[] bytes) {
+		Formatter formatter = new Formatter();
+		for (byte b : bytes) {
+			formatter.format("%02x", b);
+		}
+		String resultado = formatter.toString();
+		formatter.close();
+		return resultado;
+	}
+
+	public void salvarHashEmArquivo(String hash, File destino) {
+		try {
+			FileUtils.writeStringToFile(destino, hash, "UTF-8");
+			log.info("Hash SHA-512 salvo no arquivo com sucesso: {}", destino.getAbsolutePath());
+		} catch (Exception e) {
+			log.error("Erro ao salvar o hash no arquivo", e);
+			throw new RuntimeException("Erro ao salvar hash", e);
+		}
+	}
+}
