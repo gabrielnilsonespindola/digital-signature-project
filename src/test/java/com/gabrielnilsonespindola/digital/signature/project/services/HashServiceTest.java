b61e79434815f67938362ad4334b30209ca00f9c
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-07-12 15:09:07 -0300

    arquivos copiados para test em resources test

--- src/test/java/com/gabrielnilsonespindola/digital/signature/project/services/HashServiceTest.java
+++ src/test/java/com/gabrielnilsonespindola/digital/signature/project/services/HashServiceTest.java
@@ -1,5 +1,7 @@
 package com.gabrielnilsonespindola.digital.signature.project.services;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,36 +11,41 @@ import digital.signature.project.services.HashService;
 
 @ExtendWith(MockitoExtension.class)
 public class HashServiceTest {
-	
+
 	@InjectMocks
 	HashService hashService;
-	
+
+	private File carregarArquivo(String nomeArquivo) throws Exception {
+		return new File(getClass().getClassLoader().getResource("arquivos/" + nomeArquivo).toURI());
+	}
+
 	@Nested
 	class calcularHashSHA512 {
-		
+
 		@Test
-		void gerarArquivoHashSHA512Corretamente() {
+		void gerarArquivoHashSHA512Corretamente() throws Exception {
+			
 			
 		}
-		
+
 		@Test
-		void ErroAoCalcularOHashSHA512DoArquivo() {
-			
+		void erroAoCalcularOHashSHA512DoArquivo() {
+
 		}
-		
+
 	}
-	
+
 	@Nested
 	class saveHashToFile {
-		
+
 		@Test
-		void salvarHashParaArquivoFuncionando() {
-			
+		void salvarHashParaArquivoFuncionando() throws Exception {
+
 		}
-		
+
 		@Test
-		void SalvarHashEmArquivoNãoFunciona() {
-			
+		void salvarHashEmArquivoNãoFunciona() {
+
 		}
 	}
 
